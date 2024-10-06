799da384cd18e34f1b4313bc8dda6d3897a9c266
Nicolas Duarte <nicolas_duarterj@proton.me>
2024-10-06 14:28:26 -0300

    add stay alive

--- app.js
+++ app.js
@@ -8,10 +8,10 @@ const usersRouter = require('./controllers/users');
 const loginRouter = require('./controllers/login');
 const contentRouter = require('./controllers/content');
 const tokensRouter = require('./controllers/tokens');
+const miscRouter = require('./controllers/misc');
 
 mongoose.set('strictQuery', false);
-console.log('Conectando ao MongoDB');
-//Lembre-se de configurar as variáveis de ambiente.
+console.log('Conectando ao MongoDB...');
 mongoose
     .connect(process.env.MONGODB_URL)
     .then(() => {
@@ -35,6 +35,8 @@ app.use('/api/content', contentRouter);
 app.use('/api/tokens', middleware.extractToken);
 app.use('/api/tokens', middleware.extractUser);
 app.use('/api/tokens', tokensRouter);
+
+app.use('/api/misc', miscRouter);
 
 app.use(middleware.unknownEndpoint);
 app.use(middleware.errorHandler);
