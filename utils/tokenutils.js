d6c1697b4b20be723ec74edf63a6b28466556ff9
Nicolas Duarte <nicolas_duarterj@proton.me>
2024-10-08 00:22:35 -0300

    prettier

--- utils/tokenutils.js
+++ utils/tokenutils.js
@@ -6,18 +6,17 @@ const getUserLearnChainTokens = async (signature, userWalletAddress) => {
         headers: { 'x-api-key': process.env.ABAKHUS_KEY },
     });
     const allTokenIDs = allTokenIDsResponse.data.ret;
-    const tokenMetadataPromiseArray = allTokenIDs.map((tokenId) => 
+    const tokenMetadataPromiseArray = allTokenIDs.map((tokenId) =>
         axios.get(`${process.env.ABAKHUS_URL}/getMetadataByTokenId`, {
             params: { owner: userWalletAddress, tokenId },
             headers: { 'x-api-key': process.env.ABAKHUS_KEY },
-        })
+        }),
     );
     const allTokenMetadataResponse = await Promise.all(tokenMetadataPromiseArray);
     const learnChainTokensMetadata = allTokenMetadataResponse
         .map((response) => response.data.metadata)
         .filter((metadata) => metadata.platform === 'LearnChain');
     return learnChainTokensMetadata;
-
-}
+};
 
 module.exports = { getUserLearnChainTokens };
